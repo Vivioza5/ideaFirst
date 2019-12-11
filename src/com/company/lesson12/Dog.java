77573d253fdaf214bccb1a8a13edb6ceaa2bcc12
Vitaliy <Vitaliyvivian12@gmail.com>
2019-12-11 21:29:13 +0200

    09.12 commit

---
+++ src/com/company/lesson12/Dog.java
@@ -0,0 +1,10 @@
+package com.company.lesson12;
+
+import com.company.lesson10.Animal;
+
+public abstract class Dog extends Animal {
+
+    public void say (){
+        System.out.println("dog say GAV");
+    }
+}
